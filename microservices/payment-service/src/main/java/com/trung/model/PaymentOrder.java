fba22012ae9edda5ee197ebe687c30fd67105955
trung1609 <vmt162005@gmail.com>
2026-03-09 15:00:39 +0700

    Add PaymentService microservice with initial setup, configuration, entities, enums, controllers, services, and Maven wrapper.

---
+++ microservices/payment-service/src/main/java/com/trung/model/PaymentOrder.java
@@ -0,0 +1,41 @@
+package com.trung.model;
+
+import com.trung.domain.PaymentMethod;
+import com.trung.domain.PaymentOrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long amount;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PaymentOrderStatus status = PaymentOrderStatus.PENDING;
+
+    @Column(nullable = false)
+    private PaymentMethod paymentMethod;
+
+    private String paymentLinkId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long bookingId;
+
+    @Column(nullable = false)
+    private Long salonId;
+}
